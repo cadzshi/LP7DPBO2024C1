05af8e1d323c6a04e48461041a2a764ac84c6222
Sabila Rosad <sabila.rosad@upi.edu>
2024-04-25 22:50:23 +0700

    upload semua

---
+++ FlappyBird/src/FlappyBird.java
@@ -0,0 +1,188 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
+
+    int frameWidth = 360;
+    int frameHeight = 640;
+
+    //image attributes
+    Image backgroundImage;
+    Image birdImage;
+    Image lowerPipeImage;
+    Image upperPipeImage;
+
+    //player
+    int playerStartPosX = frameWidth / 8;
+    int playerStartPosY = frameHeight / 2;
+    int playerWidth = 44;
+    int playerHeight = 35;
+    Player player;
+
+    // pipes atributes
+    int pipeStartPosX = frameWidth;
+    int pipeStartPosY = 0;
+    int pipeWidth = 64;
+    int pipeHeight = 512;
+    ArrayList<Pipe> pipes;
+
+    //game logic2
+    Timer gameLoop;
+    Timer pipesCooldown;
+    int gravity = 1;
+    double score = 0;
+    boolean gameOver = false;
+
+
+    //  constructor
+    public FlappyBird(){
+        setPreferredSize(new Dimension(frameWidth, frameHeight));
+        setFocusable(true);
+        addKeyListener(this);
+        //setBackground(Color.blue);
+
+        //load images
+        backgroundImage = new ImageIcon(getClass().getResource("assets/background.png")).getImage();
+        birdImage = new ImageIcon(getClass().getResource("assets/bintang.png")).getImage();
+        lowerPipeImage = new ImageIcon(getClass().getResource("assets/lowerPipe.png")).getImage();
+        upperPipeImage = new ImageIcon(getClass().getResource("assets/upperPipe.png")).getImage();
+
+        player = new Player(playerStartPosX, playerStartPosY, playerWidth, playerHeight, birdImage);
+        pipes = new ArrayList<Pipe>();
+
+        //pipes cooldown timer
+        pipesCooldown = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                placePipes();
+            }
+        });
+        pipesCooldown.start();
+
+        gameLoop = new Timer(1000/60, this);
+        gameLoop.start();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+
+    }
+    public void draw(Graphics g) {
+        // background
+        g.drawImage(backgroundImage, 0, 0, frameWidth, frameHeight, null);
+        // player
+        g.drawImage(player.getImage(), player.getPosX(), player.getPosY(), player.getWidth(), player.getHeight(), null);
+        // pipes
+        for (Pipe pipe : pipes) {
+            g.drawImage(pipe.getImage(), pipe.getPosX(), pipe.getPosY(), pipe.getWidth(), pipe.getHeight(), null);
+        }
+        // score
+        g.setColor(Color.BLACK);
+        g.setFont(new Font("Arial", Font.PLAIN, 32));
+        if (gameOver){
+            String message = "Jiakhhh Kalah...\n";
+            message += "Skor Akhirmu: " + (int) score;
+
+            // Menambahkan tombol untuk merestart game
+            Object[] options = {"Restart Game", "Keluar"};
+            int choice = JOptionPane.showOptionDialog(this, message, "Game Over", JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+
+            if (choice == JOptionPane.YES_OPTION) {
+                restartGame();
+            } else {
+                System.exit(0);
+            }
+        } else {
+            g.drawString(String.valueOf((int)score), 10, 35);
+        }
+
+    }
+    public void placePipes(){
+        int randomPipePosY = (int) (pipeStartPosY - pipeHeight/4 - Math.random() * (pipeHeight/2));
+        int openingSpace = frameHeight/4;
+
+        Pipe upperPipe = new Pipe(pipeStartPosX, randomPipePosY, pipeWidth, pipeHeight, upperPipeImage);
+        pipes.add(upperPipe);
+
+        Pipe lowerPipe = new Pipe(pipeStartPosX, randomPipePosY + pipeHeight + openingSpace, pipeWidth, pipeHeight, lowerPipeImage);
+        pipes.add(lowerPipe);
+    }
+    public void move(){
+        // Update player position
+        player.setVelocityY(player.getVelocityY() + gravity);
+        player.setPosY(player.getPosY() + player.getVelocityY());
+        player.setPosY(Math.max(player.getPosY(), 0));
+
+        // Update pipes position
+        for (Pipe pipe : pipes) {
+            pipe.setPosX(pipe.getPosX() + pipe.getVelocityX());
+
+            // penambahan score
+            if (!pipe.isPassed() && player.getPosX() > pipe.getPosX() + pipe.getWidth()){
+                pipe.setPassed(true);
+                score += 0.5; // 0.5 untuk satu pipa, karna ada 2 jadi bertambah 1
+            }
+            // kalau player nabrak pipa
+            if (collision(player, pipe)){
+                gameOver = true;
+            }
+        }
+
+        // kalau player nabrak batas bawah
+        if (player.getPosY() + player.getHeight() >= frameHeight) {
+            gameOver = true;
+        }
+    }
+
+    // Metode untuk me-restart permainan
+    public void restartGame() {
+        // Inisialisasi ulang atribut permainan
+        player.setPosY(playerStartPosY);
+        pipes.clear();
+        // Mulai kembali timer permainan dan timer pipesCooldown
+        gameLoop.start();
+        pipesCooldown.start();
+        // set gameOver menjadi false dan score menjadi 0
+        gameOver = false;
+        score = 0;
+    }
+    public boolean collision(Player a, Pipe b){
+        return a.getPosX() < b.getPosX() + b.getWidth() &&
+                a.getPosX() + a.getWidth() > b.getPosX() &&
+                a.getPosY() < b.getPosY() + b.getHeight() &&
+                a.getPosY() + a.getHeight() > b.getPosY();
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+        move();
+        repaint();
+        if (gameOver) {
+            pipesCooldown.stop();
+            gameLoop.stop();
+        }
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e){
+        if (e.getKeyCode() == KeyEvent.VK_SPACE){
+            player.setVelocityY(-10);
+        } else if (e.getKeyCode() == KeyEvent.VK_R) {
+            restartGame();
+        }
+    }
+    @Override
+    public void keyTyped(KeyEvent e){
+
+    }
+    @Override
+    public void keyReleased(KeyEvent e){
+
+    }
+}
